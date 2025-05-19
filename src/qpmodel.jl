1429c604958ee021ab6c84e40a1a896aa02fae1d
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-05-19 00:21:16 -0500

    Update qpmodel.jl

--- src/qpmodel.jl
+++ src/qpmodel.jl
@@ -389,7 +389,7 @@ end
 
 linobj(qp::AbstractQuadraticModel, args...) = qp.data.c
 
-function NLPModels.objgrad!(qp::QuadraticModel, x::AbstractVector, g::AbstractVector)
+function NLPModels.objgrad!(qp::AbstractQuadraticModel, x::AbstractVector, g::AbstractVector)
   NLPModels.increment!(qp, :neval_obj)
   NLPModels.increment!(qp, :neval_grad)
     
@@ -408,7 +408,7 @@ function NLPModels.objgrad!(qp::QuadraticModel, x::AbstractVector, g::AbstractVe
   return f, g
 end
 
-function NLPModels.obj(qp::QuadraticModel, x::AbstractVector)
+function NLPModels.obj(qp::AbstractQuadraticModel, x::AbstractVector)
   NLPModels.increment!(qp, :neval_obj)
   if qp.meta.islp
     f = qp.data.c0 + dot(qp.data.c, x)
@@ -422,7 +422,7 @@ function NLPModels.obj(qp::QuadraticModel, x::AbstractVector)
   return f
 end
 
-function NLPModels.grad!(qp::QuadraticModel, x::AbstractVector, g::AbstractVector)
+function NLPModels.grad!(qp::AbstractQuadraticModel, x::AbstractVector, g::AbstractVector)
   NLPModels.increment!(qp, :neval_grad)
   if qp.meta.islp
     g .= qp.data.c
@@ -463,7 +463,7 @@ function fill_structure!(S::SparseMatrixCSC, rows, cols)
 end
 
 function NLPModels.hess_structure!(
-  qp::QuadraticModel{T, S, M1},
+  qp::AbstractQuadraticModel{T, S, M1},
   rows::AbstractVector{<:Integer},
   cols::AbstractVector{<:Integer},
 ) where {T, S, M1 <: SparseMatrixCOO}
@@ -475,7 +475,7 @@ function NLPModels.hess_structure!(
 end
 
 function NLPModels.hess_structure!(
-  qp::QuadraticModel{T, S, M1},
+  qp::AbstractQuadraticModel{T, S, M1},
   rows::AbstractVector{<:Integer},
   cols::AbstractVector{<:Integer},
 ) where {T, S, M1 <: SparseMatrixCSC}
@@ -492,7 +492,7 @@ function NLPModels.hess_structure!(
 end
 
 function NLPModels.hess_structure!(
-  qp::QuadraticModel{T, S, M1},
+  qp::AbstractQuadraticModel{T, S, M1},
   rows::AbstractVector{<:Integer},
   cols::AbstractVector{<:Integer},
 ) where {T, S, M1 <: Matrix}
@@ -518,7 +518,7 @@ function fill_coord!(S::SparseMatrixCSC, vals, obj_weight)
 end
 
 function NLPModels.hess_coord!(
-  qp::QuadraticModel{T, S, M1},
+  qp::AbstractQuadraticModel{T, S, M1},
   x::AbstractVector{T},
   vals::AbstractVector{T};
   obj_weight::Real = one(eltype(x)),
@@ -533,7 +533,7 @@ function NLPModels.hess_coord!(
 end
 
 function NLPModels.hess_coord!(
-  qp::QuadraticModel{T, S, M1},
+  qp::AbstractQuadraticModel{T, S, M1},
   x::AbstractVector{T},
   vals::AbstractVector{T};
   obj_weight::Real = one(eltype(x)),
@@ -548,7 +548,7 @@ function NLPModels.hess_coord!(
 end
 
 function NLPModels.hess_coord!(
-  qp::QuadraticModel{T, S, M1},
+  qp::AbstractQuadraticModel{T, S, M1},
   x::AbstractVector{T},
   vals::AbstractVector{T};
   obj_weight::Real = one(eltype(x)),
@@ -579,7 +579,7 @@ NLPModels.hess_coord!(
 ) = hess_coord!(qp, x, vals, obj_weight = obj_weight)
 
 function NLPModels.jac_lin_structure!(
-  qp::QuadraticModel{T, S, M1, M2},
+  qp::AbstractQuadraticModell{T, S, M1, M2},
   rows::AbstractVector{<:Integer},
   cols::AbstractVector{<:Integer},
 ) where {T, S, M1, M2 <: SparseMatrixCOO}
@@ -676,7 +676,7 @@ function NLPModels.cons_lin!(qp::AbstractQuadraticModel, x::AbstractVector, c::A
 end
 
 function NLPModels.hprod!(
-  qp::QuadraticModel,
+  qp::AbstractQuadraticModel,
   x::AbstractVector,
   v::AbstractVector,
   Hv::AbstractVector;
